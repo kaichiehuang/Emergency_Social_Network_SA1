f49026af5ec5fff417f3cfd8476af4ad38af29a2
yib <bo.yi@sv.cmu.edu>
2020-02-01 13:49:25 -0800

    db migration (prepare collections of users and reserved_names)

---
+++ migration/migrations/20200201070737-users.js
@@ -0,0 +1,17 @@
+module.exports = {
+  async up(db) {
+    await db.createCollection('users');
+    db.collection('users').insertOne({
+      username: "username",
+      password: "encrypted password",
+      name: "NAME",
+      last_name: "family name",
+      acknowledgement: false
+    });
+  },
+
+  async down(db) {
+    await db.collection('users').deleteMany({});
+    await db.collection('users').drop();
+  }
+};
