6f3384a755f29ef5b89c5e06ff7c2a3f18b5a528
yib <bo.yi@sv.cmu.edu>
2020-02-01 14:02:59 -0800

    db migration (prepare collections of users and reserved_names)

---
+++ migration/migrate-mongo-config.js
@@ -0,0 +1,25 @@
+const config = {
+  mongodb: {
+    // Change (or review) the url to your MongoDB:
+    url: "mongodb://localhost:27017",
+
+    // our database name:
+    databaseName: "esn_sa1",
+
+    options: {
+      useNewUrlParser: true, // removes a deprecation warning when connecting
+      useUnifiedTopology: true, // removes a deprecating warning when connecting
+      //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
+      //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
+    }
+  },
+
+  // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
+  migrationsDir: "migrations",
+
+  // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
+  changelogCollectionName: "changelog"
+};
+
+// Return the config as a promise
+module.exports = config;
