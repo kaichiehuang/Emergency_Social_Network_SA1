1848804ab7b57b2b7c1098035a7013d7ada202ab
Juan Pablo Lozano Gómez <jplozgom@gmail.com>
2020-02-03 23:24:27 -0800

    merge complete

--- migration/migrations/20200201194501-reserved_names.js
+++ migration/migrations/20200201194501-reserved_names.js
@@ -1,24 +1,28 @@
 module.exports = {
-  //TODO Bo will learn promise and await
-  async up(db) {
-    await db.createCollection('reserved_names');
-    const fs = require('fs').promises;
-    const data = await fs.readFile('migrations/reserved_username_list.txt','utf-8');
-    const name_arr = [];
-    for (item of data.split('\n')) {
-      if (item.length != 1) {
-        var words = item.split(' ');
-        for (word of words) {
-          name_arr.push({'name': word});
-        }
-      }
+    //TODO Bo will learn promise and await
+    async up(db) {
+        db.createCollection('reserved_names');
+        const fs = require('fs');
+        console.log(fs);
+        fs.readFile('migrations/reserved_username_list.txt', 'utf-8',function read(err, data) {
+            console.log(data);
+            const name_arr = [];
+            for (item of data.split('\n')) {
+                if (item.length != 1) {
+                    var words = item.split(' ');
+                    for (word of words) {
+                        name_arr.push({
+                            'name': word
+                        });
+                    }
+                }
+            }
+            const collection = db.collection('reserved_names');
+            collection.insertMany(name_arr);
+        });
+    },
+    async down(db) {
+        await db.collection('reserved_names').deleteMany({});
+        await db.collection('reserved_names').drop();
     }
-    const collection = db.collection('reserved_names');
-    collection.insertMany(name_arr);
-  },
-
-  async down(db) {
-    await db.collection('reserved_names').deleteMany({});
-    await db.collection('reserved_names').drop();
-  }
 };
