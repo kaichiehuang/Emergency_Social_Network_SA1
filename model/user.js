7f2282f9397b9636738c1a3d76223af3d4e2d8de
Juan Pablo Lozano Gómez <jplozgom@gmail.com>
2020-02-08 22:46:48 -0800

    pass all validations logic to validate function and everything from route to controller

--- model/user.js
+++ model/user.js
@@ -1,7 +1,7 @@
 const UserModel = require('./model').UserMongo;
 const ReservedNamesModel = require('./model').ReservedNamesMongo;
 const bcrypt = require('bcrypt');
-
+const blacklist = require("the-big-username-blacklist");
 class User {
     constructor(username, password, name, last_name) {
         this.username = username;
@@ -10,69 +10,81 @@ class User {
         this.last_name = last_name;
         this.acknowledgement = false;
     }
-
-    async validate() {
-        let usrRes = this.validateUserName(this.username);
-        let pwdRes = this.validatePassword(this.password);
-
-        if (!usrRes.res) {
-            return usrRes;
-        } else if (!pwdRes.res) {
-            return pwdRes;
-        } else {
-
-            let passRes = await this.isPasswordMatch();
-            if (!passRes.res) {
-                return passRes;
-            }
-            return usrRes;
-        }
+    /**
+     * [validate description]
+     * @return {[type]} [description]
+     */
+    validate() {
+        return new Promise((resolve, reject) => {
+            //validate username structure
+            this.validateUserName(this.username).then(result => {
+                console.log("Username structure validated");
+                //validate banned username
+                return this.validateBannerUsername();
+            }).then(result => {
+                console.log("Banned username validated");
+                //validate password structure
+                return this.validatePassword();
+            }).then(result => {
+                console.log("Password structure validated");
+                //validate user vs DB
+                return this.isPasswordMatch();
+            }).then(result => {
+                console.log("Username and password validated vs DB ---- All validations passed");
+                //if no errors resolve promise with result obj
+                resolve(result);
+            }).catch(function(err) {
+                console.log("validations not passed");
+                //if errors reject the promise
+                console.log(err);
+                reject(err);
+            });
+        })
     }
-
-
     //WITH PROMISES
-    isPasswordMatch(password){
-        return new Promise((resolve, reject)=>{
+    /**
+     * [isPasswordMatch description]
+     * @param  {[type]}  password [description]
+     * @return {Boolean}          [description]
+     */
+    isPasswordMatch(password) {
+        return new Promise((resolve, reject) => {
             let resObj = {
                 res: true,
                 msg: ''
             };
-
-            UserModel.find({'username': this.username}).exec()
-                .then(userFind => {
-                    if (userFind.length !== 0) {
-
-                        bcrypt.compare(this.password, userFind[0].password, function(err, res) {
-                            if(res) {
-                                resolve(userFind[0]);
-                            } else {
-                                resObj.res = false;
-                                resObj.msg = 'password/username not matched ';
-                            }
-                        });
-                    }else{
-                        resObj.res = true;
-                        resolve(resObj);
-                    }
-
-                })
-                .catch(err =>  reject(err));
-
+            UserModel.find({
+                'username': this.username
+            }).exec().then(userFind => {
+                if (userFind.length !== 0) {
+                    bcrypt.compare(this.password, userFind[0].password, function(err, res) {
+                        if (res) {
+                            resolve(userFind[0]);
+                        } else {
+                            resObj.res = false;
+                            resObj.msg = 'password/username not matched ';
+                        }
+                    });
+                } else {
+                    resObj.res = true;
+                    resolve(resObj);
+                }
+            }).catch(err => reject(err));
         })
-
-
     }
-
-
-
+    /**
+     * [isReservedNames description]
+     * @return {Boolean} [description]
+     */
     isReservedNames() {
         //check reserved name
         let resObj = {
             res: true,
             msg: ''
         };
-        ReservedNamesModel.find({'name': this.username})
-        .then(names => {
+        ReservedNamesModel.find({
+            'name': this.username
+        }).then(names => {
             if (names.length != 0) {
                 resObj.res = false;
                 resObj.msg = 'Invalid username, is in the list of reserved usernames.';
@@ -84,31 +96,32 @@ class User {
             });
         });
     }
-
-
     //VALIDATE USER NAMES LENGTH
+    /**
+     * [validateUserName description]
+     * @return {[type]} [description]
+     */
     validateUserName() {
-        return new Promise((resolve, reject)=>{
+        return new Promise((resolve, reject) => {
             let resObj = {
                 res: true,
                 msg: ''
             };
-
             if (this.username.length < 3) {
                 resObj.res = false;
                 resObj.msg = 'Invalid username, please enter a longer username';
                 reject(resObj);
             }
-
             resolve(resObj);
         })
     }
-
-
-
     //VALIDATE PASSWORD  LENGTH
+    /**
+     * [validatePassword description]
+     * @return {[type]} [description]
+     */
     validatePassword() {
-        return new Promise((resolve, reject)=>{
+        return new Promise((resolve, reject) => {
             let resObj = {
                 res: true,
                 msg: ''
@@ -122,15 +135,13 @@ class User {
                 resObj.msg = '';
                 resolve(resObj);
             }
-
         })
     }
-
     /**
      * [registerUser description]
      * @return {[type]} [description]
      */
-     registerUser() {
+    registerUser() {
         let hash = this.hashPassword(this.password);
         let newUser = new UserModel({
             username: this.username,
@@ -139,59 +150,72 @@ class User {
             last_name: this.last_name,
             acknowledgement: false
         });
-
         return newUser.save();
     }
-
     /**
      * [updateKnowledge description]
      * @param  {[type]} userId          [description]
      * @param  {[type]} acknowledgement [description]
      * @return {[type]}                 [description]
      */
-    updateKnowledge(userId,acknowledgement){
-
-        return UserModel.findByIdAndUpdate(userId,{$set:{acknowledgement:acknowledgement}},{new:true});
-
+    updateKnowledge(userId, acknowledgement) {
+        return UserModel.findByIdAndUpdate(userId, {
+            $set: {
+                acknowledgement: acknowledgement
+            }
+        }, {
+            new: true
+        });
     };
-
     /**
      * [findUserById description]
      * @param  {[type]} userId [description]
      * @return {[type]}        [description]
      */
-     findUserById(userId) {
-        UserModel.findById(userId)
-        .then(res => {
+    findUserById(userId) {
+        UserModel.findById(userId).then(res => {
             return res;
-        })
-        .catch(err => {
+        }).catch(err => {
             return err;
         });
     }
-
     /**
      * [hashPassword description]
      * @param  {[type]} password [description]
      * @return {[type]}          [description]
      */
-     hashPassword(password) {
+    hashPassword(password) {
         return bcrypt.hashSync(password, 10);
     }
-
+    /**
+     * [validateBannerUsername description]
+     * @return {[type]} [description]
+     */
+    validateBannerUsername() {
+        return new Promise((resolve, reject) => {
+            var resObj = {};
+            //3. Validate BlackListUser\
+            let black = blacklist.validate(this.username);
+            if (!black) {
+                resObj.res = false;
+                resObj.msg = 'Invalid username, this username is reserved for the platform. Please enter a different username.';
+                reject(resObj);
+            } else {
+                resObj.res = true;
+                resObj.msg = '';
+                resolve(resObj);
+            }
+        });
+    }
     /**
      * [userExist description]
      * @param  {[type]} userId [description]
      * @return {[type]}        [description]
      */
-    async userExist(userId){
-
+    async userExist(userId) {
         console.log("before calling findbyId");
-        let userInfo =  await UserModel.findById(userId);
+        let userInfo = await UserModel.findById(userId);
         return userInfo._id;
     };
-
-
 }
-
 module.exports = User;
