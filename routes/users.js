60bb07ce8ed9bb5151a96a6346f1452eeabae62f
yib <bo.yi@sv.cmu.edu>
2020-02-17 12:22:15 -0800

    chat_message migration; mongoose & model set up.

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const UsersController = require(__dirname + '/../controllers/usersController');
 var bodyParser = require('body-parser');
 const { validateTokenMid } = require('../middleware/tokenServer');
+const ChatMsg = require('../model/chat_message.js');
 
 // application/json parser
 var jsonParser = bodyParser.json();
@@ -23,6 +24,10 @@ var usersController = new UsersController();
   }
   * */
 router.get('/',  function(req, res, next) {
+    // console.log('----- test save chat messsages --------');
+    // ChatMsg.saveMsgTest();
+    // console.log('----- test get messsage --------');
+    // ChatMsg.getMsgTest("KOBE_user_id");
     res.send('404');
 });
 //post method for creating a user
