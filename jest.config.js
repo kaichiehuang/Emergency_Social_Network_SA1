dd8db2e773953eda815a564fc7739a7bcbddd636
davidgama-crypto <59715223+davidgama-crypto@users.noreply.github.com>
2020-03-04 19:59:28 -0800

    corrrections on test environment for parallel tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  preset: '@shelf/jest-mongodb',
+  //line used for jsest-mongodb dependency
+  //preset: '@shelf/jest-mongodb',
   clearMocks: true,
   coverageDirectory: "coverage",
   testMatch: [
     "**/test/**/*.js",
     '**/?(*.)+(spec|test).+(ts|tsx|js)',
-  ]
+  ],
+  testEnvironment: 'node'
 };
--- model/database.js
+++ model/database.js
@@ -1,19 +1,4 @@
 const mongoose = require('mongoose');
-//
-// const mongoDBURL = 'mongodb://localhost:27017/esn_sa1';
-// mongoose.connect(mongoDBURL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
-//
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'db connection error:'));
-//
-// module.exports = mongoose;
-
-
-//import mongoose from 'mongoose'
-
 
 class Database{
 
@@ -25,10 +10,5 @@ class Database{
         mongoose.connection.close()
     }
 }
-
-// export const connect = async () =>
-//     mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017')
-//
-// export const close = async () => mongoose.connection.close()
 
 module.exports = Database;
