394e8dbde1958e5a7c4050f4e54c3fc8712075e1
kaichiehuang <kaichieh@andrew.cmu.edu>
2020-04-09 06:03:38 -0700

    edit mode UI

--- public/javascripts/emergency_status_detail.js
+++ public/javascripts/emergency_status_detail.js
@@ -4,5 +4,78 @@ class EmergencyStatusDetail {
         this.user_id = null;
     }
 
+    static setEditDetailEvent() {
+        $('.editButton').click(function(event) {
+            event.preventDefault();
+            console.log("edit button clicked!");
+            EmergencyStatusDetail.generateEditPage();
+        });
+    }
+
+    static setSaveDetailEvent() {
+        $('.saveButton').click(function(event) {
+            event.preventDefault();
+            console.log('save button clicked!');
+            //TODO
+            EmergencyStatusDetail.sendStatusDetail();
+            EmergencyStatusDetail.generatePreviewPage();
+        });
+    }
+
+    static generateEditPage() {
+        //TODO: retreive detailed data
+        
+        //hide elements from preview mode
+        $("#briefDescriptionPreview").addClass("hidden");
+
+        //brief description
+        document.getElementById("briefDescriptionEdit").innerHTML = "testtesttest";
+        $("#briefDescriptionEdit").removeClass("hidden");
+
+        //share location
+        $("#shareLocationToggle").removeClass("hidden");
+        
+
+    }
+
+    static generatePreviewPage() {
+        //TODO: retreive detailed data
+
+        //hide elements from edit mode
+        $("#briefDescriptionEdit").addClass("hidden");
+        $("#shareLocationToggle").addClass("hidden");
+
+        //brief description
+        document.getElementById("briefDescriptionPreview").innerHTML = "test";
+        $("#briefDescriptionPreview").removeClass("hidden");
+    }
+
+    
+        
+
+        
+
+
+
+    
+
 
 }
+
+$(function() {
+
+    EmergencyStatusDetail.generatePreviewPage();
+
+    EmergencyStatusDetail.setEditDetailEvent();
+
+    EmergencyStatusDetail.setSaveDetailEvent();
+
+
+
+
+
+
+
+
+
+});
