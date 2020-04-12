76621e103f042e0cbb982ffe9e5e3e9bde4a63f0
kaichiehuang <kaichieh@andrew.cmu.edu>
2020-04-11 18:16:23 -0700

    generate preview page

--- public/javascripts/emergency_status_detail.js
+++ public/javascripts/emergency_status_detail.js
@@ -39,11 +39,63 @@ class EmergencyStatusDetail {
     }
 
     static generatePreviewPage() {
-        //TODO: retreive detailed data
-        
         //hide elements from edit mode
         $("#briefDescriptionEdit").addClass("hidden");
         $("#shareLocationToggle").addClass("hidden");
+
+
+        //TODO: retreive detailed data
+        let jwt = Cookies.get('user-jwt-esn');
+        let user_id = Cookies.get('user-id');
+        //get brief description and share location
+        $.ajax({
+            url: apiPath + '/emergencyStatusDetail/' + user_id,
+            type: 'get',
+            headers: {
+                "Authorization": jwt
+            }
+        }).done(function(response) {
+            console.log(response);
+
+            //brief description
+            document.getElementById("briefDescriptionPreview").innerHTML = response.status_description;
+            $("#briefDescriptionPreview").removeClass("hidden");
+
+        }).fail(function(e) {
+            $("#get-emergency-detail-alert").html(e);
+            $("#get-emergency-detail-alert").show();
+        }).always(function() {
+            console.log("complete");
+        });
+        //get picture and description
+        $.ajax({
+            url: apiPath + '/emergencyStatusDetail/picture/' + user_id,
+            type: 'get',
+            headers: {
+                "Authorization": jwt
+            }
+        }).done(function(response) {
+            console.log(response);
+
+            response.forEach(function (pictureObj) {
+                console.log(pictureObj);
+                let t = document.querySelector('#pictureAndDescriptionTemplate');
+                t.content.querySelector('img').src = pictureObj.picture_path;
+                t.content.querySelector('img').id = pictureObj._id;
+                t.content.querySelector('p').innerHTML = pictureObj.picture_description;
+
+                let clone = document.importNode(t.content, true);
+                let pictureContainer = document.getElementsByClassName('sharePicture');
+                pictureContainer[0].appendChild(clone);
+            })    
+        }).fail(function(e) {
+            $("#get-picture-and-description-alert").html(e);
+            $("#get-picture-and-description-alert").show();
+        }).always(function() {
+            console.log("complete");
+        });
+        
+        
 
         //brief description
         document.getElementById("briefDescriptionPreview").innerHTML = "test";
@@ -74,11 +126,6 @@ $(function() {
     EmergencyStatusDetail.setEditDetailEvent();
 
     EmergencyStatusDetail.setSaveDetailEvent();
-
-
-
-
-
 
 
 
