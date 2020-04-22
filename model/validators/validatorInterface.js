5a1afed1952244d5013fc9685148f109d31a6ce4
Bo Yi <bo.yi@sv.cmu.edu>
2020-04-22 03:30:52 -0700

    eslint

--- model/validators/validatorInterface.js
+++ model/validators/validatorInterface.js
@@ -2,37 +2,38 @@
  * Our class for user model taht will be attached to the schema
  */
 class ValidatorInterface {
+    // eslint-disable-next-line require-jsdoc
     constructor() {
         this.validateData = [];
         this.validatorRules = {
-            "requiredRules": [], // list of variables
-            "lengthRules": [], // array of objects {"field", "minLength": 3},{"field", "minLength": 3},
-            "customRules": [],
-            "acceptedData": []
+            'requiredRules': [], // list of variables
+            'lengthRules': [], // array of objects {"field", "minLength": 3},{"field", "minLength": 3},
+            'customRules': [],
+            'acceptedData': []
         };
     }
     /**
      *   Validates a
      */
     validateDataRules(data) {
-        //set data for global access
+        // set data for global access
         this.setValidationData(data);
         return new Promise((resolve, reject) => {
-            //1. validate required fields
+            // 1. validate required fields
             this.validateRequiredFields()
-            .then((result) => {
-                //2. validate required fields
-                return this.validateFieldsByLength();
-            })
-            .then((result) => {
-                //3. custom rules
-                return this.validateCustomRules();
-            })
-            .then((result) => {
-                return resolve(true);
-            }).catch((err) => {
-                return reject(err);
-            });
+                .then((result) => {
+                // 2. validate required fields
+                    return this.validateFieldsByLength();
+                })
+                .then((result) => {
+                // 3. custom rules
+                    return this.validateCustomRules();
+                })
+                .then((result) => {
+                    return resolve(true);
+                }).catch((err) => {
+                    return reject(err);
+                });
         });
     }
     /**
@@ -42,7 +43,7 @@ class ValidatorInterface {
     validateRequiredFields() {
         return new Promise((resolve, reject) => {
             if (this.validatorRules.requiredRules != undefined && this.validatorRules.requiredRules.length > 0) {
-                for (var i = 0; i < this.validatorRules.requiredRules.length; i++) {
+                for (let i = 0; i < this.validatorRules.requiredRules.length; i++) {
                     const field = this.validatorRules.requiredRules[i];
                     let validationResult = false;
                     if (field.innerObject != undefined) {
@@ -52,7 +53,7 @@ class ValidatorInterface {
                     }
                     if (validationResult == false) {
                         if (field.msg == undefined || field.msg.length == 0) {
-                            return reject(field.fieldName + " is a required field");
+                            return reject(field.fieldName + ' is a required field');
                         }
                         return reject(field.msg);
                     }
@@ -69,11 +70,11 @@ class ValidatorInterface {
      */
     validateRequiredField(fieldName, innerObject) {
         if (innerObject != undefined) {
-            if (this.validateData[innerObject][fieldName] == undefined || this.validateData[innerObject][fieldName] == "" || this.validateData[innerObject][fieldName].length == 0) {
+            if (this.validateData[innerObject][fieldName] == undefined || this.validateData[innerObject][fieldName] == '' || this.validateData[innerObject][fieldName].length == 0) {
                 return false;
             }
         } else {
-            if (this.validateData[fieldName] == undefined || this.validateData[fieldName] == "" || this.validateData[fieldName].length == 0) {
+            if (this.validateData[fieldName] == undefined || this.validateData[fieldName] == '' || this.validateData[fieldName].length == 0) {
                 return false;
             }
         }
@@ -88,7 +89,7 @@ class ValidatorInterface {
     validateFieldsByLength() {
         return new Promise((resolve, reject) => {
             if (this.validatorRules.lengthRules != undefined && this.validatorRules.lengthRules.length > 0) {
-                for ( var i = 0 ; i < this.validatorRules.lengthRules.length; i++) {
+                for ( let i = 0; i < this.validatorRules.lengthRules.length; i++) {
                     const field = this.validatorRules.lengthRules[i];
                     let validationResult = false;
                     let allowEmpty = false;
@@ -103,7 +104,7 @@ class ValidatorInterface {
                     }
                     if (!validationResult) {
                         if (field.msg == undefined || field.msg.length == 0) {
-                            return reject("Minimum length for " + field.fieldName + " is " + field.minLength );
+                            return reject('Minimum length for ' + field.fieldName + ' is ' + field.minLength );
                         }
                         return reject(field.msg);
                     }
@@ -120,13 +121,13 @@ class ValidatorInterface {
      * @param  {[type]} fieldName [description]
      * @return {[type]}           [description]
      */
-    validateFieldByLength (fieldName, minLength, innerObject, allowEmpty) {
-        //if it allows empty values move foward
-        if(allowEmpty && (this.validateData[fieldName] == undefined || this.validateData[fieldName].length == 0)){
+    validateFieldByLength(fieldName, minLength, innerObject, allowEmpty) {
+        // if it allows empty values move foward
+        if (allowEmpty && (this.validateData[fieldName] == undefined || this.validateData[fieldName].length == 0)) {
             return true;
         }
 
-        //else check for length rule
+        // else check for length rule
         if (innerObject != undefined) {
             if (this.validateData[innerObject][fieldName] == undefined || this.validateData[innerObject][fieldName].length < minLength) {
                 return false;
@@ -147,17 +148,17 @@ class ValidatorInterface {
     validateCustomRules() {
         return new Promise((resolve, reject) => {
             if (this.validatorRules.customRules != undefined && this.validatorRules.customRules.length > 0) {
-                for (var i = 0; i < this.validatorRules.customRules.length; i++) {
+                for (let i = 0; i < this.validatorRules.customRules.length; i++) {
                     const field = this.validatorRules.customRules[i];
                     let validationResult = false;
-                    //call custom function
-                    if(field.customRuleName.length > 0){
+                    // call custom function
+                    if (field.customRuleName.length > 0) {
                         validationResult = this[field.customRuleName]();
                     }
 
                     if (!validationResult) {
                         if (field.msg == undefined) {
-                            return reject("Error");
+                            return reject('Error');
                         }
                         return reject(field.msg);
                     }
