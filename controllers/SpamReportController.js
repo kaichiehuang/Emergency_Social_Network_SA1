60f9ed6d5f2f9e5de1a82f9f4ffdd1764aa661dd
Bo Yi <bo.yi@sv.cmu.edu>
2020-04-26 17:52:37 -0700

    refactor

--- controllers/SpamReportController.js
+++ controllers/SpamReportController.js
@@ -13,75 +13,67 @@ class SpamReportController {
      * @param req
      * @param res
      */
-    createSpamReport(req, res) {
+    async createSpamReport(req, res) {
         const requestData = req.body;
-        const level = requestData['level'] === 'message' ?
-            constants.MESSAGE_LEVEL_SPAM: constants.USER_LEVEL_SPAM;
+        const level = requestData['level'] === 'message' ? constants.MESSAGE_LEVEL_SPAM: constants.USER_LEVEL_SPAM;
         const type = requestData['type'];
         const description = requestData['description'];
         const newReport = new SpamReport(level, type, description);
-        const currentUserId = requestData['current_user_id'];
-
         if (level === constants.USER_LEVEL_SPAM) {
-            const reportedUserId = requestData['reported_user_id'];
-            let spamUser = false;
-            let spamUserReportedTimes;
-            User.setReportSpam(reportedUserId, currentUserId)
-                .then((user) => {
-                    spamUser = user.spam;
-                    spamUserReportedTimes = user.reported_spams.size;
-                    return newReport.saveSpamReport();
-                })
-                .then((newReport) => {
-                    let spamData ={
-                        'user': {
-                            'user_id': reportedUserId,
-                            'spam': spamUser,
-                            'number': spamUserReportedTimes
-                        }
-                    };
-                    const socketIO = new SocketIO(res.io);
-                    socketIO.emitMessage('spam-report-number', spamData);
-                    // res.io.emit('spam-report-number', spam);
-                    res.contentType('application/json');
-                    res.status(201).send(newReport);
-                })
-                .catch((err) => {
-                    /* istanbul ignore next */
-                    return res.status(422).send({
-                        'error': err.message
-                    });
-                });
+            await SpamReportController.handleUserlevel(requestData, newReport, res);
         } else {
-            const reportMessageId = requestData['message_id'];
-            let spamMessage = false;
-            let spamMessageReportedTimes;
-            ChatMessage.setReportSpam(reportMessageId, currentUserId)
-                .then((message) => {
-                    spamMessage = message.spam;
-                    spamMessageReportedTimes = message.reported_spams.size;
-                    return newReport.saveSpamReport();
-                })
-                .then((newReport) => {
-                    let spamData = {
-                        'message': {
-                            'message_id': reportMessageId,
-                            'spam': spamMessage,
-                            'number': spamMessageReportedTimes
-                        }
-                    };
-                    const socketIO = new SocketIO(res.io);
-                    socketIO.emitMessage('spam-report-number', spamData);
-                    res.contentType('application/json');
-                    res.status(201).send(newReport);
-                })
-                .catch((err) => {
-                    /* istanbul ignore next */
-                    return res.status(422).send({
-                        'error': err.message
-                    });
-                });
+            await SpamReportController.handleMsgLevel(requestData, newReport, res);
         }
+    }
+
+    /**
+     * handle user level spam
+     * @param requestData
+     * @param newReport
+     */
+    static async handleUserlevel(requestData, newReport, res) {
+        const reportedUserId = requestData['reported_user_id'];
+        const user = await User.setReportSpam(reportedUserId, requestData['current_user_id']);
+        const spamUser = user.spam;
+        const spamUserReportedTimes = user.reported_spams.size;
+        const report = await newReport.saveSpamReport();
+        const spamData ={
+            'user': {
+                'user_id': reportedUserId,
+                'spam': spamUser,
+                'number': spamUserReportedTimes
+            }
+        };
+        const socketIO = new SocketIO(res.io);
+        socketIO.emitMessage('spam-report-number', spamData);
+        res.contentType('application/json');
+        res.status(201).send(report);
+    };
+
+    /**
+     * handle msg level
+     * @param requestData
+     * @param newReport
+     * @param res
+     * @returns {Promise<void>}
+     */
+    static async handleMsgLevel(requestData, newReport, res) {
+        const reportMessageId = requestData['message_id'];
+        const msg = await ChatMessage.setReportSpam(reportMessageId, requestData['current_user_id']);
+        const spamMessage = msg.spam;
+        const spamMessageReportedTimes = msg.reported_spams.sizes;
+        const report = await newReport.saveSpamReport();
+        const spamData = {
+            'message': {
+                'message_id': reportMessageId,
+                'spam': spamMessage,
+                'number': spamMessageReportedTimes
+            }
+        };
+        const socketIO = new SocketIO(res.io);
+        socketIO.emitMessage('spam-report-number', spamData);
+        res.contentType('application/json');
+        res.status(201).send(report);
     }
 }
 module.exports = SpamReportController;
